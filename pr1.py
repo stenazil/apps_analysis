6d24ad11ad0306fcf48b59cc2a9326e5b3f15f80
stenazil <60507262+stenazil@users.noreply.github.com>
2020-04-05 20:48:29 +0600

    Add files via upload

--- pr1.py
+++ pr1.py
@@ -1,1 +1,391 @@
-# work_things
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Analyze data of Play Market and App Store 
+# 
+# *This project is about analyzing ratings, users, etc. The source of the database that we use are **Play Market** - store of applications for the Android system and **AppStore** - store of applications for iPhone users.*
+# 
+# **The goal** *of this project is helping a company looking for the most suitable apps and audience of this app for sale company's ad that can increase recognition.*
+
+# In[1]:
+
+
+from csv import reader
+
+### The Google Play data set ###
+opened_file = open('googleplaystore.csv')
+read_file = reader(opened_file)
+android = list(read_file)
+android_header = android[0]
+android = android[1:]
+
+### The App Store data set ###
+opened_file = open('AppleStore.csv')
+read_file = reader(opened_file)
+ios = list(read_file)
+ios_header = ios[0]
+ios = ios[1:]
+        
+
+
+# In[2]:
+
+
+def explore_data(dataset, start, end, rows_and_columns=False):
+    dataset_slice = dataset[start:end]    
+    for row in dataset_slice:
+        print(row)
+        print('\n') # adds a new (empty) line between rows
+        
+    if rows_and_columns:
+        print('Number of rows:', len(dataset))
+        print('Number of columns:', len(dataset[0]))
+
+print(android_header)
+print('\n')
+explore_data(android, 0, 3, True)
+
+
+# In[3]:
+
+
+print(ios_header)
+print('\n')
+explore_data(ios, 0, 3, True)
+
+
+# In[4]:
+
+
+print(android[10472])  # incorrect row
+print('\n')
+print(android_header)  # header
+print('\n')
+print(android[0])      # correct row
+
+
+# In[5]:
+
+
+print(len(android))
+del android[10472]  # don't run this more than once
+print(len(android))
+
+
+# In[6]:
+
+
+for app in android:
+    name = app[0]
+    if name == 'Instagram':
+        print(app)
+
+
+# In[7]:
+
+
+duplicate_apps = []
+unique_apps = []
+
+for app in android:
+    name = app[0]
+    if name in unique_apps:
+        duplicate_apps.append(name)
+    else:
+        unique_apps.append(name)
+    
+print('Number of duplicate apps:', len(duplicate_apps))
+print('\n')
+print('Examples of duplicate apps:', duplicate_apps[:15])
+
+
+# In[8]:
+
+
+reviews_max = {}
+
+for app in android:
+    name = app[0]
+    n_reviews = float(app[3])
+    
+    if name in reviews_max and reviews_max[name] < n_reviews:
+        reviews_max[name] = n_reviews
+        
+    elif name not in reviews_max:
+        reviews_max[name] = n_reviews
+
+
+# In[9]:
+
+
+print('Expected length:', len(android) - 1181)
+print('Actual length:', len(reviews_max))
+
+
+# In[10]:
+
+
+android_clean = []
+already_added = []
+
+for app in android:
+    name = app[0]
+    n_reviews = float(app[3])
+    if (reviews_max[name] == n_reviews) and (name not in already_added):
+        android_clean.append(app)
+        already_added.append(name) # make sure this is inside the if block 
+
+
+# In[11]:
+
+
+explore_data(android_clean, 0, 3, True)
+
+
+# In[12]:
+
+
+print(ios[813][1])
+print(ios[6731][1])
+
+print(android_clean[4412][0])
+print(android_clean[7940][0])
+
+
+# In[13]:
+
+
+def is_english(string):
+    non_ascii = 0
+    
+    for character in string:
+        if ord(character) > 127:
+            non_ascii += 1
+    
+    if non_ascii > 3:
+        return False
+    else:
+        return True
+
+
+# In[14]:
+
+
+android_english = []
+ios_english = []
+
+for app in android_clean:
+    name = app[0]
+    if is_english(name):
+        android_english.append(app)
+        
+for app in ios:
+    name = app[1]
+    if is_english(name):
+        ios_english.append(app)
+        
+explore_data(android_english, 0, 3, True)
+print('\n')
+explore_data(ios_english, 0, 3, True)
+
+
+# In[15]:
+
+
+android_final = []
+ios_final = []
+
+for app in android_english:
+    price = app[7]
+    if price == '0':
+        android_final.append(app)
+        
+for app in ios_english:
+    price = app[4]
+    if price == '0.0':
+        ios_final.append(app)
+        
+print(len(android_final))
+print(len(ios_final))
+
+
+# To minimize risks and overhead, our validation strategy for an app idea is comprised of three steps:
+# 
+#   - *Build a minimal Android version of the app, and add it to Google Play.*
+#   - *If the app has a good response from users, we develop it further.*
+#   - *If the app is profitable after six months, we build an iOS version of the app and add it to the App Store.*
+#   
+# Because our end goal is to add the app on both Google Play and the App Store, we need to find app profiles that are successful on both markets. For instance, a profile that works well for both markets might be a productivity app that makes use of gamification.
+
+# In[16]:
+
+
+def freq_table(dataset, index):
+    table = {}
+    total = 0
+    for row in dataset:
+        total += 1
+        value = row[index]
+        if value in table:
+            table[value] += 1
+        else:
+            table[value] = 1
+            
+    table_persentages = {}
+    for key in table:
+        persentages = (table[key] / total) * 100
+        table_persentages[key] = persentages
+    return table_persentages
+
+def display_table(dataset, index):
+    table = freq_table(dataset, index)
+    table_display = []
+    for key in table:
+        key_val_as_tuple = (table[key], key)
+        table_display.append(key_val_as_tuple)
+        
+    table_sorted = sorted(table_display, reverse = True)
+    for entry in table_sorted:
+        print(entry[1], ':', entry[0])
+    
+
+
+# In[17]:
+
+
+display_table(ios_final, -5)
+
+
+#  We can see that among the free English apps in AppStore, more than a half (58.16%) are games. Entertainment apps are close to 8%, followed by photo and video apps, which are close to 5%. Only 3.66% of the apps are designed for education, followed by social networking apps which amount for 3.29% of the apps in our data set.
+#  The general impression is that App Store (at least the part containing free English apps) is dominated by apps that are designed for fun (games, entertainment, photo and video, social networking, sports, music, etc.), while apps with practical purposes (education, shopping, utilities, productivity, lifestyle, etc.) are more rare.
+# 
+
+# In[18]:
+
+
+display_table(android_final, 1) #category
+
+
+# In[19]:
+
+
+display_table(android_final, -4)
+
+
+# The difference between **the Genres** and **the Category** columns is not crystal clear, but one thing we can notice is that the Genres column is much more granular (it has more categories). We're only looking for the bigger picture at the moment, so we'll only work with the Category column moving forward.
+# 
+# Up to this point, we found that the App Store is dominated by apps designed for fun, while Google Play shows a more balanced landscape of both practical and for-fun apps. Now we'd like to get an idea about the kind of apps that have most users.
+
+# In[20]:
+
+
+genres_ios = freq_table(ios_final, -5)
+
+for genre in genres_ios:
+    total = 0
+    len_genre = 0
+    for app in ios_final:
+        genre_app = app[-5]
+        if genre_app == genre:            
+            n_ratings = float(app[5])
+            total += n_ratings
+            len_genre += 1
+    avg_n_ratings = total / len_genre
+    print(genre, ':', avg_n_ratings)
+
+
+# On average, navigation apps have the highest number of user reviews, but this figure is heavily influenced by Waze and Google Maps, which have close to half a million user reviews together:
+
+# In[21]:
+
+
+for app in ios_final:
+    if app[-5] == 'Navigation':
+        print(app[1], ':', app[5]) #print name and number of ratings
+        
+
+
+# 
+# The same pattern applies to social networking apps, where the average number is heavily influenced by a few giants like Facebook, Pinterest, Skype, etc. Same applies to music apps, where a few big players like Pandora, Spotify, and Shazam heavily influence the average number.
+
+# Our aim is to find popular genres, but navigation, social networking or music apps might seem more popular than they really are. The average number of ratings seem to be skewed by very few apps which have hundreds of thousands of user ratings, while the other apps may struggle to get past the 10,000 threshold. We could get a better picture by removing these extremely popular apps for each genre and then rework the averages, but we'll leave this level of detail for later.
+# 
+# Reference apps have 74,942 user ratings on average, but it's actually the Bible and Dictionary.com which skew up the average rating:
+
+# In[22]:
+
+
+for app in ios_final:
+    if app[-5] == 'Reference':
+        print(app[1], ':', app[5])
+        
+
+
+# However, this niche seems to show some potential. One thing we could do is take another popular book and turn it into an app where we could add different features besides the raw version of the book. This might include daily quotes from the book, an audio version of the book, quizzes about the book, etc. On top of that, we could also embed a dictionary within the app, so users don't need to exit our app to look up words in an external app.
+# 
+# This idea seems to fit well with the fact that the App Store is dominated by for-fun apps. This suggests the market might be a bit saturated with for-fun apps, which means a practical app might have more of a chance to stand out among the huge number of apps on the App Store.
+# 
+# Other genres that seem popular include weather, book, food and drink, or finance. The book genre seem to overlap a bit with the app idea we described above, but the other genres don't seem too interesting to us:
+# 
+#  - Weather apps — people generally don't spend too much time in-app, and the chances of making profit from in-app adds are low. Also, getting reliable live weather data may require us to connect our apps to non-free APIs.
+# 
+# - Food and drink — examples here include Starbucks, Dunkin' Donuts, McDonald's, etc. So making a popular food and drink app requires actual cooking and a delivery service, which is outside the scope of our company.
+# 
+# - Finance apps — these apps involve banking, paying bills, money transfer, etc. Building a finance app requires domain knowledge, and we don't want to hire a finance expert just to build an app.
+# 
+# Now we will analyze the Google Play market a bit.
+# 
+# ## Most Popular Apps by Genre on Google Play
+# 
+# For the Google Play market, we actually have data about the number of installs, so we should be able to get a clearer picture about genre popularity. However, the install numbers don't seem precise enough — we can see that most values are open-ended (100+, 1,000+, 5,000+, etc.):
+# 
+# 
+
+# In[23]:
+
+
+display_table(android_final, 5)
+
+
+# In[24]:
+
+
+
+categories_android = freq_table(android_final, 1)
+
+for category in categories_android:
+    total = 0
+    len_category = 0
+    for app in android_final:
+        category_app = app[1]
+        if category_app == category:            
+            n_installs = app[5]
+            n_installs = n_installs.replace(',', '')
+            n_installs = n_installs.replace('+', '')
+            total += float(n_installs)
+            len_category += 1
+    avg_n_installs = total / len_category
+    print(category, ':', avg_n_installs)
+
+
+# On average, communication apps have the most installs: 38,456,119. 
+
+# In[25]:
+
+
+for app in android_final:
+    if app[1] == 'BOOKS_AND_REFERENCE' and (app[5] == '1,000,000,000+'
+                                            or app[5] == '500,000,000+'
+                                            or app[5] == '100,000,000+'):
+        print(app[0], ':', app[5])
+
+
+# This niche seems to be dominated by software for processing and reading ebooks, as well as various collections of libraries and dictionaries, so it's probably not a good idea to build similar apps since there'll be some significant competition.
+# 
+# We also notice there are quite a few apps built around the book Quran, which suggests that building an app around a popular book can be profitable. It seems that taking a popular book (perhaps a more recent book) and turning it into an app could be profitable for both the Google Play and the App Store markets
+# 
+
+# ##  Conclusions
+# In this project, we analyzed data about the App Store and Google Play mobile apps with the goal of recommending an app profile that can be profitable for both markets.
+# 
+# We concluded that taking a popular book (perhaps a more recent book) and turning it into an app could be profitable for both the Google Play and the App Store markets. The markets are already full of libraries, so we need to add some special features besides the raw version of the book. This might include daily quotes from the book, an audio version of the book, quizzes on the book, a forum where people can discuss the book, etc.
